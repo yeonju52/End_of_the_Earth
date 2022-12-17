c5e73faad7ddc73977744bf0b27b2cb79d8c3bfe
yeonjuLee <yeonju509@gmail.com>
2022-12-17 19:36:43 +0900

    add Alien Move to multiple Targets

--- Assets/Script/Alien_Move.cs
+++ Assets/Script/Alien_Move.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class Alien_Move : MonoBehaviour
 {
+    public GameObject parent;
     public GameObject target;
     private Animator animator;
+    private int i = 0;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        target = parent.transform.GetChild(i % 3).gameObject;
+        Debug.Log(target);
     }
 
     void Update()
     {
-        if (target.transform.position != Vector3.MoveTowards(transform.position, target.transform.position, 0.5f))
+        target = parent.transform.GetChild(i % 3).gameObject;
+        target.SetActive(true);
+        Debug.Log(i);
+        if (i / 3 > 3)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.5f);
-
-            transform.LookAt(target.transform);
-            //Debug.Log(Vector3.MoveTowards(transform.position, target.transform.position, 1f));
-
-            animator.SetBool("off", false);
+            animator.SetBool("off", true);
+            i++;
         }
         else
         {
-            animator.SetBool("off", true);
+            if (target.transform.position != Vector3.MoveTowards(transform.position, target.transform.position, 0.1f))
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.1f);
+
+                transform.LookAt(target.transform);
+                //Debug.Log(Vector3.MoveTowards(transform.position, target.transform.position, 0.1f));
+
+                animator.SetBool("off", false);
+            }
+            else
+            {
+                target.SetActive(false);
+                i++;
+            }
         }
     }
 }
+
+/*
+    if (target.transform.position != Vector3.MoveTowards(transform.position, target.transform.position, 0.1f))
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 0.1f);
+
+        transform.LookAt(target.transform);
+        //Debug.Log(Vector3.MoveTowards(transform.position, target.transform.position, 0.1f));
+
+        animator.SetBool("off", false);
+    }
+    else
+    {
+        animator.SetBool("off", true);
+} 
+ */
