0172b866df6bafbdc2d0f603cf90853706a9b36d
yeonjuLee <yeonju509@gmail.com>
2022-12-18 22:41:33 +0900

    Update FPS Swimming Script
    Solve: Terrain 밑으로 안 떨어짐
    Bug: 오브젝트와 충돌 인식 못함

--- Assets/Script/Swimming.cs
+++ Assets/Script/Swimming.cs
@@ -10,42 +10,55 @@ namespace UnityStandardAssets.Characters.FirstPerson
     public class Swimming : MonoBehaviour
     {
         public Transform target;
-        public float swimSpeed = 10f;
+        public float swimSpeed = 5f;
         [SerializeField] private MouseLook m_MouseLook;
+        public float surface;
 
         void Start()
         {
-            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().useGravity = false;    // false;
             m_MouseLook.Init(transform, target.transform);
             ResetVelocity();
         }
 
         private void Update()
         {
+            surface = Terrain.activeTerrain.SampleHeight(transform.position) +0.05f;
+            if (transform.position.y > surface)
+            {
+                transform.position -= target.up * swimSpeed * 0.5f * Time.deltaTime;
+                Debug.Log(surface);
+            }
+            //else
+            //{
+            //    transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position), transform.position.y);
+            //}
             RotateView();
             isSwimming();
             isFloating();
+            ResetVelocity();
         }
 
+        
         private void FixedUpdate()
         {
             m_MouseLook.UpdateCursorLock();
         }
 
-        private void OnTriggerStay(Collider other)
-        {
-            if (other.name == "SwimmingArea")
-            {
-                transform.position -= target.up * swimSpeed * 0.5f * Time.deltaTime;
-                Debug.Log("enter");
-                ResetVelocity();
-            }
-        }
+        //private void OnTriggerStay(Collider other)
+        //{
+        //    if (other.name == "SwimmingArea")
+        //    {
+        //        transform.position -= target.up * swimSpeed * 0.5f * Time.deltaTime;
+        //        ResetVelocity();
+        //        Debug.Log("enter");
+        //    }
+        //}
 
-        private void OnTriggerExit(Collider other)
-        {
-            Debug.Log("exit");
-        }
+        //private void OnTriggerExit(Collider other)
+        //{
+        //    Debug.Log("exit");
+        //}
 
         void isSwimming()
         {
@@ -74,7 +87,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
                 transform.position += target.up * swimSpeed * Time.deltaTime;
             }
         }
-
         void ResetVelocity()
         {
             GetComponent<Rigidbody>().velocity = Vector3.zero;
