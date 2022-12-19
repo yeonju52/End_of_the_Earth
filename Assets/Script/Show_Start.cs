35b06811f9d8a852d40d46362f618713aed7d7fd
yeonjuLee <yeonju509@gmail.com>
2022-12-19 11:33:03 +0900

    update UI

---
+++ Assets/Script/Show_Start.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Show_Start : MonoBehaviour
+{
+    public GameObject ui;
+    private int turn = 0;
+    private AudioSource beep;
+    private GameObject player;
+
+    private void Start()
+    {
+        ui.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("u"))
+        {
+            ui.SetActive(false);
+        }
+    }
+}
